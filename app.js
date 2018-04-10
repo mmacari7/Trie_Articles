cb04204add4f5c834df842cdcd7d07db1b072765
jtlafleur <jtlafleur15@gmail.com>
2018-04-10 12:54:30 -0400

    takes out a, and, an, the, etc

--- app.js
+++ app.js
@@ -109,14 +109,14 @@ function parseArticle(article, numOfCompanies, t) {
                 const threeWords = t.find(article.slice(index, index + 3).join(' '));
                 if (threeWords[0] !== -1) {
                     hitCount[threeWords[0]]++;
-                    totalWords++; // What do we do with this
+                    totalWords++; 
                     index += 2;
                     continue;
                 }
             }
             if (twoWords[0] !== -1) {
                 hitCount[twoWords[0]]++;
-                totalWords++; // What do we do with this
+                totalWords++;
                 index++;
                 continue;
             }
@@ -124,10 +124,11 @@ function parseArticle(article, numOfCompanies, t) {
         if (oneWord[0] !== -1) {
             hitCount[oneWord[0]]++;
         }
-        totalWords++;
+        if (!/a|an|and|the|or|but/.test(article[index]))
+            totalWords++;
     }
-    console.log('hitCount:', hitCount);
-    console.log('TotalWords:', totalWords);
+    // console.log('hitCount:', hitCount);
+    // console.log('TotalWords:', totalWords);
 
     return [hitCount, totalWords];
 }
@@ -162,7 +163,6 @@ function main() {
 
     //Print Results
     printResult(companies, totalWords, hitCount);
-
 }
 
 main();
